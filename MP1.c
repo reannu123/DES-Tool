d864eebc4fce58b8f51fa40cda7dc1ef79998dbb
reannu123 <reannumon123@gmail.com>
2022-03-23 13:22:18 +0000

    Expansion for DES function implemented

--- MP1.c
+++ MP1.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 
-int CipherBlocks[2][64];
-int FPText[2][64];
+
 // split string into 2 lines
 // Plaintext = first line
 // Key = 2nd line
@@ -25,7 +24,7 @@ int FPText[2][64];
         // Shift Table 
 
 // DES (Block 1 and 2)
-    // Initial Permutation
+    // DONE Initial Permutation
     //Round Function
         // Binary split into 32 bits R and L
         // R to DES Function
@@ -36,7 +35,20 @@ int FPText[2][64];
         // Xor with L, store to R-temp
         // R stored to L, R-temp stored to R
         // Next rounds
-    // Final Permutation
+    // DONE Final Permutation
+
+int CipherBlocks[2][64];
+int FPText[2][64];
+
+// For Key Generation
+int RoundKeys[16][64];
+
+// For Round Function
+int LRound[32];
+int RRound[32];
+int ExpBlock[48];
+int StraightBlock[32];
+int RTemp[32]; // For Xoring
 
 
 int IPTable[] =      // Initial Permutation Table
@@ -63,6 +75,38 @@ int FPTable[] =      // Final Permutation Table
     33, 1, 41,  9, 49, 17, 57, 25
 };
 
+int ExpDBoxTable[] =
+{
+    32,  1,  2,  3,  4,  5,
+    4,  5,  6,  7,  8,  9,
+    8,  9, 10, 11, 12, 13,
+    12, 13, 14, 15, 16, 17,
+    16, 17, 18, 19, 20, 21,
+    20, 21, 22, 23, 24, 25,
+    24, 25, 26, 27, 28, 29,
+    28, 29, 30, 31, 32,  1
+};
+
+int Sbox1[4][16] = {
+    14,  4, 13,  1,  2, 15, 11,  8,  3, 10,  6, 12,  5,  9,  0,  7,
+    0, 15,  7,  4, 14,  2, 13,  1, 10,  6, 12, 11,  9,  5,  3,  8,
+    4,  1, 14,  8, 13,  6,  2, 11, 15, 12,  9,  7,  3, 10,  5,  0,
+    15, 12,  8,  2,  4,  9,  1,  7,  5, 11,  3, 14, 10,  0,  6, 13
+};
+
+int SBoxPermutation(int SBoxInput[], int SBoxNum){
+    int row = SBoxInput[0]*2 + SBoxInput[5];
+    int column = SBoxInput[1]*8 + SBoxInput[2]*4 + SBoxInput[3]*2 + SBoxInput[4];
+
+
+    
+}
+
+int ExpansionDBox(){
+    for(int i = 0; i < 48; i++){
+        ExpBlock[i] = RRound[ExpDBoxTable[i]-1];
+    }
+}
 
 int initialPermutation(char input[]){
     for(int i=0; i<64; i++){
@@ -78,6 +122,9 @@ int finalPermutation(){
 
 char* des(char input[], char key[]){
     initialPermutation(input);
+    for(int i=0; i<16; i++){
+        // round( ,)
+    }
     finalPermutation();
 
     return "Hello";
@@ -90,14 +137,15 @@ int main(){
     printf("%s\n", input);
     des(input,key);
 
-    for (int i = 0; i < 64; i++){
-        printf("%d",CipherBlocks[0][i]);
-    }
-    printf("\n");
+    // for (int i = 0; i < 64; i++){
+    //     printf("%d",CipherBlocks[0][i]);
+    // }
+    // printf("\n");
     
-    for (int i = 0; i < 64; i++){
-        printf("%d",FPText[0][i]);
-    }
-    printf("\n");
+    // for (int i = 0; i < 64; i++){
+    //     printf("%d",FPText[0][i]);
+    // }
+    //printf("\n");
     //printf("Cipher Text: %s\n", ciphertext);
+    printf("S-box: %d",Sbox1[0][15]);
 }
